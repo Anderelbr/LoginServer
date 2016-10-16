369b436eb6bc9838946a83ab6f8e5f9280a3944c
Anderelbr <anderelbr20@gmail.com>
2016-10-16 00:36:26 -0300

    Update AuthServer.js

--- LoginServer/AuthServer.js
+++ LoginServer/AuthServer.js
@@ -7,24 +7,30 @@ var io = require ('socket.io')(http);
 var port = process.env.PORT || 7100;
 
 app.get('/', function(req, res){
-	res.send("Server is running on port : " + port);
+	app.send("Server is running on port : " + port);
+	InitializeServer();
 });
 
-io.on ('connection', function (socket){
-	
-var NewClient = new Login.Start(socket);
-	
-socket.on ("LoginEnterReq", function(){
-console.log("New connection");
-socket.emit("LoginEnterRes");
-});
+//Start Server
+var InitializeServer = function () {
+	//Receive Connections
+	io.on ('connection', function (socket){
+		var NewClient = new Login.Start(socket);
+		StartEvents(NewClient);	
+	});
+};
 
-socket.on ('ConfirmAccountReq', NewClient.Confirm);
-socket.on ('disconnect', function (){
-console.log ('Client disconnected');
-	
-});	
-});
+function StartEvents (NewClient){
+	socket.on ("LoginEnterReq", function(){
+		console.log("New connection");
+		socket.emit("LoginEnterRes");
+	});
+
+	socket.on ('ConfirmAccountReq', NewClient.Confirm);
+	socket.on ('disconnect', function (){
+		console.log ('Client disconnected');
+	});
+};
 
 http.listen(port, function(){
 	console.log("Html server is running on port : " + port);
