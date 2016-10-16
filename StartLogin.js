07e1a7d93e1d1041a7612c2ae11cb499332c873f
Anderelbr <anderelbr20@gmail.com>
2016-10-15 23:30:13 -0300

    Update StartLogin.js

--- StartLogin.js
+++ StartLogin.js
@@ -1,3 +1,37 @@
-var AuthServer = require ('./LoginServer/AuthServer.js');
+var Login = require ('./Login.js');
 
-console.log ('-- Server is running on port : 7100 --');
+var app = require('express')();
+var html = require('html').Server(app);
+var io = require ('socket.io')(html);
+
+var port = process.env.PORT || 7100;
+
+app.get('/', function(req, res){
+	app.send("Server is running on port : " + port);
+	InitializeServer();
+});
+
+//Start Server
+var InitializeServer = function () {
+	//Receive Connections
+	io.on ('connection', function (socket){
+		var NewClient = new Login.Start(socket);
+		StartEvents(NewClient);	
+	});
+};
+
+function StartEvents (NewClient){
+	socket.on ("LoginEnterReq", function(){
+		console.log("New connection");
+		socket.emit("LoginEnterRes");
+	});
+
+	socket.on ('ConfirmAccountReq', NewClient.Confirm);
+	socket.on ('disconnect', function (){
+		console.log ('Client disconnected');
+	});
+};
+
+html.listen(port, function(){
+	console.log("Html server is running on port : " + port);
+});
