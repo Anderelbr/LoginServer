ae62b19de541a2dc274f982b0145fc5f39c43e79
Anderelbr <anderelbr20@gmail.com>
2016-10-15 23:36:23 -0300

    Update StartLogin.js

--- StartLogin.js
+++ StartLogin.js
@@ -1,20 +1,3 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var AuthServer = require ('./LoginServer/AuthServer.js');
 
-var port = process.env.PORT || 7100;
-
-app.get('/', function(request, response) {
-  response.send("<h1>Sapbe</h1>");
-});
-
-io.on ('connection', function(socket){
-	socket.on ("LoginEnterReq", function(){
-		console.log("New connection");
-		socket.emit("LoginEnterRes");
-	});
-});
-
-http.listen(port, function(){
-	console.log("sapbe");
-});
+console.log ('-- Server is running on port : 7100 --');
