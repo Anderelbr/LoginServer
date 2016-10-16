e1b8f4851e051e68c12ede5642041a9c78e6cae8
Anderelbr <anderelbr20@gmail.com>
2016-10-16 17:54:33 -0300

    Update AuthServer.js

--- LoginServer/AuthServer.js
+++ LoginServer/AuthServer.js
@@ -8,17 +8,13 @@ var port = process.env.PORT || 7100;
 
 app.get('/', function(req, res){
 	res.send("Server is running on port : " + port);
-	InitializeServer();
 });
 
-//Start Server
-var InitializeServer = function () {
-	//Receive Connections
-	io.on ('connection', function (socket){
-		var NewClient = new Login.Start(socket);
-		StartEvents(NewClient);	
-	});
-};
+//Receive Connections
+io.on ('connection', function (socket){
+	var NewClient = new Login.Start(socket);
+	StartEvents(NewClient);	
+});
 
 function StartEvents (NewClient){
 	socket.on ("LoginEnterReq", function(){
